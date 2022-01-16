4ccf492546cd4fb4163bfae31d118da32c3178db
DuStein <steindu@gmail.com>
2022-01-16 20:00:19 -0300

    ajustando arquivos

--- src/server.ts
+++ src/server.ts
@@ -6,4 +6,8 @@ app.get("/", (req, res) => {
     res.json({message: "OK GET raiz"})
 })
 
+app.get("/go", (req, res) => {
+    res.json({pasta:"GO"})
+})
+
 app.listen(3030, ()=> console.log("SERVIDOR ativo!"))
