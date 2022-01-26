8d28dce93a9771dceb1f36a4036da1810624c5ee
Eduardo DuStein <68870425+dustein@users.noreply.github.com>
2022-01-26 16:08:25 -0300

    continuando no VS virtual

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,24 @@ import "reflect-metadata";
 import express from "express";
 import { router } from "./routes";
 import "./database";
-
+import { Request, Response, NextFunction } from "express";
 const app = express();
 
 app.use(express.json());
 
 app.use(router);
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+    if(err instanceof Error) {
+        return response.status(400).json({error: err.message})
+    } 
+
+    return response.status(500).json({
+        status: "error",
+        message: "Internal Server Error"
+    })
+    //instalar biblioteca express-async-errors
+})
 
 
 app.listen(3030, ()=> console.log("SERVIDOR ativo!"))
