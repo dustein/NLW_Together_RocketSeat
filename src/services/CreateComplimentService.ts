570fd68a5e255b9110f492436324b007bb0fe97c
DuStein <steindu@gmail.com>
2022-02-25 15:31:51 -0300

    avancando

---
+++ src/services/CreateComplimentService.ts
@@ -0,0 +1,40 @@
+import { getCustomRepository, getRepository } from "typeorm";
+import { ComplimentsRepositories } from "../repositories/ComplimentsRepositories";
+import { UsersRepositories } from "../repositories/UsersRepositories";
+
+interface IComplimentRequest {
+    tag_id: string;
+    user_sender: string;
+    user_receiver: string;
+    message: string;
+}
+
+class CreateComlimentService {
+    async execute({tag_id, user_receiver, user_sender, message}: IComplimentRequest) {
+
+        const complimentRepositories = getCustomRepository(ComplimentsRepositories);
+        const usersRepositories = getCustomRepository(UsersRepositories);
+
+        const userReceiverExists = await usersRepositories.findOne(user_receiver);
+
+        if (user_sender === user_receiver) {
+            throw new Error("Usuário não pode enviar para si mesmo")
+        }
+        if (!userReceiverExists) {
+            throw new Error("User receiver não existe ! ")
+        }
+
+        const compliment = complimentsRepositories.create({
+            tag_id,
+            user_receiver,
+            user_sender,
+            message
+        })
+
+        await complimentsRepositories.save(compliment);
+
+
+    }
+}
+
+export { CreateComlimentService };
