775e3f6588263bf957888321c27895f527bb8c8a
DuStein <steindu@gmail.com>
2022-02-25 15:13:44 -0300

    manyToOne

--- src/middlewares/ensureAdmin.ts
+++ src/middlewares/ensureAdmin.ts
@@ -1,6 +1,10 @@
 import { Request, Response, NextFunction } from "express";
 
-export function ensureAdmin(request: Request, response: Response, next: NextFunction) {
+export function ensureAdmin(
+    request: Request,
+    response: Response,
+    next: NextFunction
+    ) {
 
     const admin = true;
 
